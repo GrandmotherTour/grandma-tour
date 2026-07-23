93f0749943e1bde1b15ad287c72812b455491245
yeji702 <popsing12@hs.ac.kr>
2026-07-24 00:55:34 +0900

    feat: 지역 목록 조회 API 구현

--- grandma-tour-back/app.js
+++ grandma-tour-back/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const regionRouter = require('./features/region/region.route');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/regions', regionRouter);
 
 // 기본 라우트
 app.get('/', (req, res) => {
